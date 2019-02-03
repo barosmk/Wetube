f716167cba4cd8654a337516a97d00bc4b75dc49
barosmk <barosmk@gmail.com>
2019-02-04 02:50:48 +0900

    router complete

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { userRouter } from "./router";
+
+const app = express()
+
+const handleHome = (req, res) => res.send('Hello from my ass');
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+    console.log('Between');
+    next();
+};
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const middleware = (req, res, next) =>{
+    res.send('not happening');
+};
+
+app.get("/", handleHome);
+app.get("profile", handleProfile);
+app.use("/user", userRouter);
+
+export default app;
