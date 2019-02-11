da231f34f1233139b5d118fa16d3717e2b006740
barosmk <barosmk@gmail.com>
2019-02-11 17:52:44 +0900

    uploading videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,14 @@
-import { videos } from "../db";
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async(req, res) => {
+  try{
+    const videos = await Video.find({}); // await cannot used without async
+    res.render("home", { pageTitle: "Home", videos });
+  } catch(error){
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -14,16 +20,19 @@ export const search = (req, res) => {
 
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
-export const postUpload = (req, res) => {
+
+export const postUpload = async(req, res) => {
   const {
-    body: {
-      file,
-      title,
-      description
-    }
-    // To Do: Upload and save video
+    body: { title, description },
+    file: { path }
   } = req;
-  res.redirect(routes.videoDetail(324393));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
